0050f44b45b3afc261df69b26da6f9d4bb71c6e1
VladTca <votsats@gmail.com>
2024-05-19 23:08:35 +0200

    🎨 Improve structure / format of the code.

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ function App() {
         <div className="App">
             <Title>Styled-components <span>.attrs</span> method</Title>
 
+            <Car topCars={topCars}/>
             <Form>
+                <Field pad="50px" placeholder="HEllo" title="help"/>
                 <Field/>
                 <Field/>
-                <Field/>
+
+                <Checkbox/>
             </Form>
         </div>
     );
@@ -18,23 +21,76 @@ function App() {
 
 export default App;
 
+
+const topCars = [
+    {id: 1, manufacturer: 'BMW', model: 'm5cs'},
+    {id: 2, manufacturer: 'Mercedes', model: 'e63s'},
+    {id: 3, manufacturer: 'Audi', model: 'rs6'}
+]
+
+type CarType = {
+    id: number
+    manufacturer: string
+    model: string
+}
+
+type CarPropsType = {
+    topCars: CarType[]
+}
+
+
+const Car = (props: CarPropsType) => {
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>
+                        manufacturer
+                    </th>
+                    <th>
+                        model
+                    </th>
+                </tr>
+                {props.topCars.map((car) =>
+                    <tr key={car.id}>
+                        <td>{car.manufacturer}</td>
+                        <td>{car.model}</td>
+                    </tr>)}
+            </table>
+        </div>
+    )
+}
+
+
 const Title = styled.h1`
     font-size: 1.5em;
     text-align: center;
     margin-bottom: 32px;
+
     span {
-      color: #e91e63;
+        color: #e91e63;
     }
 `;
 
 const Form = styled.form`
-  width: 100%;
-  max-width: 500px;
+    width: 100%;
+    max-width: 500px;
 `;
 
-const Field = styled.input`
-  padding: 5px 15px;
-  margin: 10px 0;
-  width: 100%;
-  font-size: 1rem;
+const Field = styled.input.attrs(({placeholder, title}) => ({
+    placeholder: placeholder || 'type smth...',
+    title: title || "hello"
+}))<{ pad?: string }>`
+    //neobazatelnoe svoistvo -?-
+    padding: 5px ${props => props.pad || '10px'}px;
+    margin: 10px 0;
+    width: 100%;
+    font-size: 1rem;
 `;
+
+const Checkbox = styled(Field).attrs((props) => ({
+    type: 'checkbox',
+    title: props.title || "Checkbox"
+
+}))``
+
